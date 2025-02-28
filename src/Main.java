fa297cdf8c226da3135b3ba9cc75881858ad8303
ali7324 <qafarali91@gmail.com>
2025-03-01 02:36:06 +0400

    comitlerin birlesmesi Squash
    this is my First commit squash

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
         System.out.println("kod 1 kod 2 kod 3");
         System.out.println(1+2);
         int a = 10 ;
+        System.out.println("sout");
+        System.out.println("sout2");
+        System.out.println("sout3");
     }
 
 }
